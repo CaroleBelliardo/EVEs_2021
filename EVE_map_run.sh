e8e50fbcb93a5e423067260b2cac3bea0c0ac683
Carole Belliardo <belliardo.carole@hotmail.fr>
2021-03-24 18:26:37 +0100

    Update EVE_map_run.sh
    path modification

--- EVE_map_run.sh
+++ EVE_map_run.sh
@@ -1,12 +1,12 @@
 !# /bin/bash
 
 cd $1
-genome = $2
+genome=$2
 GT_path=""
 fastq_path=""
-sam_path = ""
-bam_path = ""
-sorted_bam_path = "" 
+sam_path=""
+bam_path=""
+sorted_bam_path="" 
 bed_path=""
 tab_path=""
 
@@ -15,13 +15,13 @@ for i in *.fna; do bowtie2-build  --large-index -f${genome} ${genome}.bwt; done 
 for i in *.fna; do hisat2-build --large-index -p 25  ${genome} ${genome}.hst; done  #longs
 
 for cond1 in F_G F_S M_G M_S; do 
-cond2 = $(echo $cond1| sed 's/_//')
-bowtie2 --threads 25 -x ${GT_path}/${genome}_GT.fna.bwt -U ${fastq_path}/${genome}/${genome}/${genome}_${cond1}_sRNA_f_t.fq -S ${sam_path}/${genome}_${cond2}_small.SAM --no-unal 
+cond2=$(echo $cond1| sed 's/_//')
+bowtie2 --threads 25 -x ${GT_path}/${genome}_GT.fna.bwt -U ${fastq_path}/${genome}_${cond1}_sRNA_f_t.fq -S ${sam_path}/${genome}_${cond2}_small.SAM --no-unal 
 done 
 
 for cond1 in F_G F_S M_G M_S; do 
-cond2 = $(echo $cond1| sed 's/_//')
-/mnt/65To/bin/hisat2-2.1.0/hisat2 -p 25 -x ${GT_path}/${genome}_GT.fna.hst -q -1 ${fastq_path}/${genome}/${genome}/${genome}_${cond1}_RNAseq_f_t.1.fq -2 ${fastq_path}/${genome}/${genome}/${genome}_${cond1}_RNAseq_f_t.2.fq --rna-strandness FR -S ${sam_path}/${genome}_${cond2}_long.SAM --no-unal 
+cond2=$(echo $cond1| sed 's/_//')
+/mnt/65To/bin/hisat2-2.1.0/hisat2 -p 25 -x ${GT_path}/${genome}_GT.fna.hst -q -1 ${fastq_path}/${genome}_${cond1}_RNAseq_f_t.1.fq -2 ${fastq_path}/${genome}_${cond1}_RNAseq_f_t.2.fq --rna-strandness FR -S ${sam_path}/${genome}_${cond2}_long.SAM --no-unal 
 done 
 
 for l in small long; do  
