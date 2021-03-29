08c0229989cec21a1d4dbbfc80d78d1899988f3c
Carole Belliardo <belliardo.carole@hotmail.fr>
2021-03-29 22:51:43 +0200

    Update and rename treePlot to treePlot.R
    modif space

--- treePlot.R
+++ treePlot.R
@@ -16,21 +16,19 @@ library(ape)
 par(mfrow=c(1,3))
 ## read tree from string
 text.string<-
-  "((Limulus polyphemus,(Centruroides exilicauda,Parasteatoda tepidariorum)),((Strigamia maritima),(((Armadillidium vulgare),((Locusta migratoria),(((Oncopeltus fasciatus), Acyrthosiphon pisum),(((Bombus_terrestris), Apis_mellifera),((Nicrophorus vespilloides, (Tribolium castaneum, Diabrotica virgifera)),
+  "((Limulus_polyphemus,(Centruroides_exilicauda,Parasteatoda_tepidariorum)),((Strigamia_maritima),(((Armadillidium_vulgare),((Locusta_migratoria),(((Oncopeltus_fasciatus), Acyrthosiphon_pisum),(((Bombus_terrestris), Apis_mellifera),((Nicrophorus_vespilloides, (Tribolium_castaneum, Diabrotica_virgifera)),
   ((Plutella_xylostella,(Heliconius_melpomene)),
   (Aedes_aegypti,(Musca_domestica,((Drosophila_virilis), Drosophila_melanogaster))))))))))));"
 
 vert.tree<-read.tree(text=text.string)
-plot(vert.tree)
-
+plot(vert.tree,no.margin=TRUE)
 
 
 ## ---
+labels=rev(vert.tree$tip.label)
 
-labels=c('Lpol','Cexi','Ptep', 'Smar', 'Avul','Lmig', 'Ofas', 'Apis',
-'Bter', 'Nves', 'Tcas', 'Dvir','Pxyl', 'Hmel', 'Aaeg',
- 'Dviri', 'Dmel')
-l=rev(labels)
+#labels=c('Lpol','Cexi','Ptep', 'Smar', 'Avul','Lmig', 'Ofas', 'Apis','Bter', 'Nves', 'Tcas', 'Dvir','Pxyl', 'Hmel', 'Aaeg','Dviri', 'Dmel')
+
 
 
 ## plot nb eve
